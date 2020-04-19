010c51da065ce363b89fadbb0e84d20e9410ed8c
LyoSU <mail@lyo.su>
2020-04-19 21:26:48 +0300

    0.0.6 fixed

--- api/query.js
+++ api/query.js
@@ -3,6 +3,8 @@ const moment = require('moment')
 module.exports = async (ctx) => {
   let query = {}
 
+  console.log(ctx.props)
+
   if (ctx.props.range) {
     query.date = {
       $gte: moment.utc(ctx.props.range.from),
@@ -16,11 +18,27 @@ module.exports = async (ctx) => {
 
   if (ctx.props.targets) {
     dataResult = ctx.props.targets.map((target) => {
-      const datapoints = stats.map((stat) => {
-        return [
-          stat[target.target],
-          stat.date
-        ]
+      const datapoints = []
+
+      let lastPoint = 0
+      let lastDate = 0
+      let lastDateUnix = 0
+
+      stats.forEach((stat) => {
+        const date = Math.ceil(Math.round(stat.date.getTime() / 1000) / Math.round(stats.length / 200))
+
+        if (lastDate !== date && lastPoint !== 0) {
+          datapoints.push([
+            lastPoint,
+            lastDateUnix
+          ])
+          lastPoint = 0
+          lastDate = date
+          lastDateUnix = 0
+        } else {
+          if (lastDateUnix === 0) lastDateUnix = stat.date.getTime()
+          lastPoint += stat[target.target]
+        }
       })
 
       return {
